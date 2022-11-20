0661c3c537a15d8244500f9a9d524ed73a9a512a
MrBlink <slajwith@gmail.com>
2022-11-21 00:02:05 +0530

    The Assets File is Added

---
+++ components/Navbar.jsx
@@ -0,0 +1,174 @@
+import { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import images from '../assets';
+
+import { Button } from '.';
+
+const MenuItems = ({ isMobile, active, setActive }) => {
+  const generateLink = (key) => {
+    switch (key) {
+      case 0:
+        return '/';
+      case 1:
+        return '/created-nft';
+      case 2:
+        return '/my-nft';
+      default:
+        return '/';
+    }
+  };
+
+  return (
+    <ul
+      className={`list-none flexCenter flex-row ${
+        isMobile && 'flex-col h-full'
+      }`}
+    >
+      {['Explore NFTs', 'Listed NFTs', 'My NFTs'].map((item, i) => (
+        <li
+          key={i}
+          onClick={() => setActive(item)}
+          className={`flex flex-row items-center font-poppins font-semibold
+          text-base dark:hover:text-white hover:text-nft-dark mx-3 ${
+            active === item
+              ? 'dark:text-white text-nft-black-1'
+              : 'dark:text-nft-gray-3 text-nft-gray-2'
+          }`}
+        >
+          <Link href={generateLink(i)}>{item}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const ButtonGroup = ({ setActive, router }) => {
+  const isConnected = false;
+
+  return isConnected ? (
+    <Button
+      btnName="Create"
+      classStyles="mx-2 rounded-xl"
+      handleClick={() => {
+        setActive('');
+
+        router.push('create-nft');
+      }}
+    />
+  ) : (
+    <Button
+      btnName="Connect"
+      classStyles="mx-2 rounded-xl"
+      handleClick={() => {}}
+    />
+  );
+};
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+  const router = useRouter();
+  const [active, setActive] = useState('Explore NFTs');
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="flexBetween w-full fixed p-4 border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
+      <div className="flex flex-1 flex-row justify-start">
+        <Link href="/">
+          <div
+            className="flexStart md:hidden cursor-pointer"
+            onClick={() => {}}
+          >
+            <Image
+              src={images.logo02}
+              height={32}
+              width={32}
+              objectFit="contain"
+              alt="CryptoKet Logo"
+            />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-2">
+              CryptoKet
+            </p>
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="hidden md:flex" onClick={() => {}}>
+            <Image
+              src={images.logo02}
+              height={32}
+              width={32}
+              objectFit="contain"
+              alt="CryptoKet Logo"
+            />
+          </div>
+        </Link>
+      </div>
+
+      <div className="flex flex-initial flex-row justify-end">
+        <div className="flex items-center mr-2">
+          <input
+            type="checkbox"
+            className="checkbox"
+            id="checkbox"
+            onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          />
+          <label
+            htmlFor="checkbox"
+            className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label"
+          >
+            <i className="fas fa-sun" />
+            <i className="fas fa-moon" />
+            <div className="w-3 h-3 absolute bg-white rounded-full ball" />
+          </label>
+        </div>
+
+        <div className="md:hidden flex">
+          <MenuItems active={active} setActive={setActive} />
+          <div className="ml-4">
+            <ButtonGroup setActive={setActive} router={router} />
+          </div>
+        </div>
+      </div>
+
+      <div className="hidden md:flex ml-2">
+        {isOpen ? (
+          <Image
+            src={images.cross}
+            objectFit="contain"
+            width={25}
+            height={25}
+            alt="menu"
+            onClick={() => setIsOpen(false)}
+            className={theme === 'light' && 'filter invert'}
+          />
+        ) : (
+          <Image
+            src={images.menu}
+            objectFit="contain"
+            width={25}
+            height={25}
+            alt="menu"
+            onClick={() => setIsOpen(true)}
+            className={theme === 'light' && 'filter invert'}
+          />
+        )}
+
+        {isOpen && (
+          <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
+            <div className="flex-1 p-4">
+              <MenuItems active={active} setActive={setActive} isMobile />
+            </div>
+            <div className="p-4 border-t dark:boder-nft-black1 border-nft-gray-1">
+              <ButtonGroup setActive={setActive} router={router} />
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ pages/create-nft.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CreateNFT = () => (
+  <div>
+    <h1 className="text-md">This is Great</h1>
+  </div>
+);
+
+export default CreateNFT;
